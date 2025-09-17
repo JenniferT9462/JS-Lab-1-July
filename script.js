887ae5bfe3b211cb75ab17b42233758de0bd012f
Jennifer Tarleton <jennifertarleton@gmail.com>
2025-09-16 21:29:09 -0500

    Added song functions

---
+++ script.js
@@ -0,0 +1,73 @@
+console.log("Hello from script.js!");
+
+// [Chorus]
+// Sunny, yesterday my life was filled with rain
+// Sunny, you smiled at me and really eased the pain
+// Now the dark days are done and the bright days are here
+// My Sunny one shines so sincere
+// Sunny one so true, I love you
+
+// [Verse 1]
+// Sunny, thank you for the sunshine bouquet
+// Sunny, thank you for the love you've brought my way
+// You gave to me your all and all
+// And now I feel ten feet tall
+// Sunny one so true, I love you
+
+// [Verse 2]
+// Sunny, thank you for the truth you let me see
+// Sunny, thank you for the facts from A to Z
+// My life was torn like wind-blown sand
+// Then a rock was formed when we held hands
+// Sunny one so true, I love you
+
+// [Verse 3]
+// Sunny, thank you for that smile upon your face
+// Mm, Sunny, thank you, thank you for that gleam that flows with grace
+// You're my spark of nature's fire
+// You're my sweet, complete desire
+// Sunny one so true, yes, I love you
+// [Outro]
+// I love you, I love you (Sunny)
+// I love you (Sunny), said, I love you (Sunny)
+// Yes, I love you (Sunny)
+function printedSong() {
+  refrain();
+  // [Verse 1]
+  console.log("Sunny, thank you for the sunshine bouquet");
+  console.log("Sunny, thank you for the love you've brought my way");
+  console.log("You gave to me your all and all");
+  console.log("And now I feel ten feet tall");
+  console.log("Sunny one so true, I love you");
+
+  // [Verse 2]
+  console.log("Sunny, thank you for the truth you let me see");
+  console.log("Sunny, thank you for the facts from A to Z");
+  console.log("My life was torn like wind-blown sand");
+  console.log("Then a rock was formed when we held hands");
+  console.log("Sunny one so true, I love you");
+
+  // [Verse 3]
+  console.log("Sunny, thank you for that smile upon your face");
+  console.log(
+    "Mm, Sunny, thank you, thank you for that gleam that flows with grace"
+  );
+  console.log("You're my spark of nature's fire");
+  console.log("You're my sweet, complete desire");
+  console.log("Sunny one so true, yes, I love you");
+
+  // [Outro]
+  console.log("I love you, I love you (Sunny)");
+  console.log("I love you (Sunny), said, I love you (Sunny)");
+  console.log("Yes, I love you (Sunny)");
+}
+printedSong();
+
+function refrain() {
+  // [Chorus]
+  console.log("Sunny, yesterday my life was filled with rain");
+  console.log("Sunny, you smiled at me and really eased the pain");
+  console.log("Now the dark days are done and the bright days are here");
+  console.log("My Sunny one shines so sincere");
+  console.log("Sunny one so true, I love you");
+}
